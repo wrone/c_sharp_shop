80004b3a0981429184ce73c3cf0bb75f98dce366
Alexey Mihailovs <Alexey Mihailovs>
2016-01-18 23:52:36 +0200

    Some functions to connect to DB added

--- c_sharp_kursa/MainWindow.xaml.cs
+++ c_sharp_kursa/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace c_sharp_kursa
 {
@@ -20,9 +22,55 @@ namespace c_sharp_kursa
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string MyConnection;
+        private MySqlConnection connection;
+        private MySqlCommand cmd;
+        private MySqlDataReader dataReader;
+
         public MainWindow()
         {
             InitializeComponent();
+            ConnectToDB("<database_ip>", "<port>", "<table_name>", "<username>", "<password>");
         }
+
+        public void ConnectToDB(string host, string port, string dbName, string user, string password)
+        {
+            MyConnection = "Server=" + host + ";Port=" + port + ";Database=" + dbName + ";Uid=" + user + ";Pwd=" + password + ";";
+            connection = new MySqlConnection(MyConnection);
+            connection.Open();
+        }
+
+        //Test query, just to know how command execution is proceeded. Function should be changed when the DB diagramm is done. 
+        public void ExecuteQuery()
+        {
+            try
+            {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO Valsts(Pilseta, Valsts) VALUES(@Pilseta, @Valsts)";
+                cmd.Parameters.AddWithValue("@Pilseta", "Ventspils");
+                cmd.Parameters.AddWithValue("@Valsts", "Latvija");
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void LoadData()
+        {
+            string text = "";
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Gramata";
+            dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                text += dataReader["<column_name>"];
+            }
+
+            dataReader.Close();
+        }
+
     }
 }
