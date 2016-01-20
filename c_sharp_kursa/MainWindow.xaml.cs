c91043b4f7da0a901e25044fd154461a0fd1f407
wrone <bykmakep@gmail.com>
2016-01-20 14:10:40 +0200

    added db connection class and user register function

--- c_sharp_kursa/MainWindow.xaml.cs
+++ c_sharp_kursa/MainWindow.xaml.cs
@@ -26,51 +26,86 @@ namespace c_sharp_kursa
         private MySqlConnection connection;
         private MySqlCommand cmd;
         private MySqlDataReader dataReader;
+        DatabaseConnection dbConn;
 
         public MainWindow()
         {
             InitializeComponent();
-            ConnectToDB("<database_ip>", "<port>", "<table_name>", "<username>", "<password>");
+            //ConnectToDB("<database_ip>", "<port>", "<table_name>", "<username>", "<password>");
+
+            dbConn = new DatabaseConnection("127.0.0.1", "3306", "mydb", "root", "root");
+
+            //List<string> list = dbConn.ReadData("SELECT * FROM Users");
+            //MessageBox.Show(list[1]);
+
+            UserRegister("da", "da", "da", "da", "da", "da");
         }
 
-        public void ConnectToDB(string host, string port, string dbName, string user, string password)
+        //public void ConnectToDB(string host, string port, string dbName, string user, string password)
+        //{
+        //    MyConnection = "Server=" + host + ";Port=" + port + ";Database=" + dbName + ";Uid=" + user + ";Pwd=" + password + ";";
+        //    connection = new MySqlConnection(MyConnection);
+        //    connection.Open();
+        //}
+
+        ////Test query, just to know how command execution is proceeded. Function should be changed when the DB diagramm is done. 
+        //public void ExecuteQuery()
+        //{
+        //    try
+        //    {
+        //        cmd = connection.CreateCommand();
+        //        cmd.CommandText = "INSERT INTO Valsts(Pilseta, Valsts) VALUES(@Pilseta, @Valsts)";
+        //        cmd.Parameters.AddWithValue("@Pilseta", "Ventspils");
+        //        cmd.Parameters.AddWithValue("@Valsts", "Latvija");
+        //        cmd.ExecuteNonQuery();
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+        //}
+
+        //public void LoadData()
+        //{
+        //    string text = "";
+        //    cmd = connection.CreateCommand();
+        //    cmd.CommandText = "SELECT * FROM Gramata";
+        //    dataReader = cmd.ExecuteReader();
+
+        //    while (dataReader.Read())
+        //    {
+        //        text += dataReader["<column_name>"];
+        //    }
+
+        //    dataReader.Close();
+        //}
+        public bool UserRegister(string name, string lastname, string login, string password, string email, string phone)
         {
-            MyConnection = "Server=" + host + ";Port=" + port + ";Database=" + dbName + ";Uid=" + user + ";Pwd=" + password + ";";
-            connection = new MySqlConnection(MyConnection);
-            connection.Open();
-        }
+            List<string> loginList = dbConn.ReadData("select Login from Users");
 
-        //Test query, just to know how command execution is proceeded. Function should be changed when the DB diagramm is done. 
-        public void ExecuteQuery()
+            if(!loginList.Contains(login))
+            {
+                dbConn.WriteData("INSERT INTO Users(Name, Lastname, Login, Password, Email, Phone, Role)"
+                               + "VALUES('" + name + "', '" + lastname + "', '" + login + "', '" 
+                               + password + "', '" + email + "', '" + phone + "', 'User')");
+
+                return true;
+            }
+            return false;
+        }
+        public bool ProductRegister(string name, string desc, string releaseDate, string endDate, string Quantity, string price, string category, string manufacturer, string picture)
         {
-            try
-            {
-                cmd = connection.CreateCommand();
-                cmd.CommandText = "INSERT INTO Valsts(Pilseta, Valsts) VALUES(@Pilseta, @Valsts)";
-                cmd.Parameters.AddWithValue("@Pilseta", "Ventspils");
-                cmd.Parameters.AddWithValue("@Valsts", "Latvija");
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            //List<string> loginList = dbConn.ReadData("select Login from Users");
+
+            //if (!loginList.Contains(login))
+            //{
+                dbConn.WriteData("INSERT INTO Users(Name, Lastname, Login, Password, Email, Phone, Role)"
+                               + "VALUES('" + name + "', '" + lastname + "', '" + login + "', '"
+                               + password + "', '" + email + "', '" + phone + "', 'User')");
+
+                return true;
+            //}
+            //return false;
         }
-
-        public void LoadData()
-        {
-            string text = "";
-            cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM Gramata";
-            dataReader = cmd.ExecuteReader();
-
-            while (dataReader.Read())
-            {
-                text += dataReader["<column_name>"];
-            }
-
-            dataReader.Close();
-        }
-
     }
 }
